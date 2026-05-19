dc028b5324e6da712695a8bfed02276cc0c5d8de
KONTAXES <admin@kontaxes.com>
2026-05-18 21:30:31 -0600

    Delete __manifest__.py

--- __manifest__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import settlement_staging
-from . import settlement
-from . import settlement_line
-from . import account_move
-from . import res_config_settings
-from . import settlement_company_account
-from . import res_partner
-from . import dashboard
